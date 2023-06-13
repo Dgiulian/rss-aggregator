b9b88a3b0a9a01e576402c4ed64653bfc8aa3aa4
Diego Giuliani <giuliani.diego@gmail.com>
2023-06-13 09:11:21 -0300

    feat: load environment variables

--- main.go
+++ main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	godotenv.Load()
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT not found in the environment")
+
+	}
+	fmt.Printf("Server started on port %v\n", portString)
 }
